fc1fa97f3c59c3aa5d0fcbcc8c15199ce9107cfd
vatsal patel <patelvatsal79715@gmail.com>
2025-10-05 11:21:09 +0530

    fix: comment out unused jQuery and Bootstrap script imports in index.html and main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,10 @@ import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'admin-lte/dist/css/adminlte.min.css';
 import $ from 'jquery'                         
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'admin-lte/dist/js/adminlte.min.js'
 
 window.$ = window.jQuery = $
-
 
 const queryClient = new QueryClient()
 
