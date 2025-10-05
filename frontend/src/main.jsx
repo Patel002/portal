c838f2cbf57f8d8377d65f935093adf39d9ae2d1
vatsal patel <patelvatsal79715@gmail.com>
2025-10-05 11:11:49 +0530

    fix: import jQuery correctly and assign it to the global window object

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'admin-lte/dist/css/adminlte.min.css';
-import 'jquery';
+import $ from 'jquery'                         
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'admin-lte/dist/js/adminlte.min.js'
+
+window.$ = window.jQuery = $
+
 
 const queryClient = new QueryClient()
 
