806b7f77573a75cf9d6830d98450f36cf03b2fb4
vatsal patel <patelvatsal79715@gmail.com>
2025-10-09 16:41:38 +0530

    fix: correct admin-lte CSS and JS import paths in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'admin-lte/dist/css/adminlte.min.css';
+import "admin-lte/dist/css/adminlte.css";
 import $ from 'jquery'                         
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import 'admin-lte/dist/js/adminlte.min.js'
+import 'admin-lte/dist/js/adminlte.js'
 import 'admin-lte';
 
 window.$ = window.jQuery = $
